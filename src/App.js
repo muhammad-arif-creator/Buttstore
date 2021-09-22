5e0af23a5e4a802ff864f45526c42ab742bc0391
Zackl-ui <bilal.maltathemes@gmail.com>
2021-09-23 02:18:36 +0500

    Login Setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import HomePage from "./pages/home";
-import Detailpage from "./components/detail_page/detail_page"
+import DetailPage from "./components/detail_page/detail_page";
+import Login from "./components/login/Login";
+import { withRouter } from "react-router-dom";
 
-function App() {
+function App(props) {
+  if (window.location.pathname !== "/" && !localStorage.getItem("isLogin")) {
+    props.history.push("/");
+  }
   return (
     <div className="App container">
       <Router>
         <Switch>
-          <Route exact path="/" component={HomePage}></Route>
-          <Route exact path="/detailpage" component={Detailpage}></Route>
+          <Route exact path="/home" component={HomePage}></Route>
+          <Route exact path="/detail-page" component={DetailPage}></Route>
+          <Route exact path="/" component={Login}></Route>
         </Switch>
       </Router>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
 import { Provider } from "react-redux";
 import store from "./store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
