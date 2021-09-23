443ee5a1dc96f22a27c1ba8d14ecef08e24e8450
rafi ullah <rafiullah372@gmail.com>
2021-09-23 22:55:32 +0500

    ..

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import HomePage from "./pages/home";
 import DetailPage from "./components/detail_page/detail_page";
 import Login from "./components/login/Login";
 import { withRouter } from "react-router-dom";
+import Nav from './components/Navbar/Nav';
+import Footer from "./components/footer/Footer";
+import Banner from "./components/Banner/Banner";
 
 function App(props) {
   if (window.location.pathname !== "/" && !localStorage.getItem("isLogin")) {
@@ -12,11 +15,14 @@ function App(props) {
   return (
     <div className="App container">
       <Router>
+        <Nav />
+        <Banner />
         <Switch>
           <Route exact path="/home" component={HomePage}></Route>
           <Route exact path="/detail-page" component={DetailPage}></Route>
           <Route exact path="/" component={Login}></Route>
         </Switch>
+        <Footer />
       </Router>
     </div>
   );
