657c945747532a5efec921b2aa924dbd3a3e1a2e
faizan-devv <faizanrizwan2277@gmail.com>
2021-09-25 05:16:49 +0500

    cart 10/10

--- src/redux/reducers/cart.reducer.js
+++ src/redux/reducers/cart.reducer.js
@@ -6,11 +6,30 @@ const cart = (state = initialState, action) => {
     switch(action.type){
         case "add":
             const newcartItem = [...state.cartItems];
-            newcartItem.push({...action.payload.product, quantity:action.payload.qty});
+            let doesExist=false;
+            for(let i=0; i<newcartItem.length;i++)
+            {
+                if(newcartItem[i].id===action.payload.product.id)
+                {
+                    doesExist=true;
+                }
+            }
+            
+            if(doesExist===false)
+            {
+                newcartItem.push({...action.payload.product, quantity:action.payload.qty});
             return {
                ...state,
                cartItems:newcartItem,
-               price:state.price+action.payload.product.price
+               price:state.price+(action.payload.product.price*action.payload.qty)
+            }
+            }
+            else{
+                alert("Product already exists in the cart");
+                return{
+                    
+                    ...state
+                }
             }
         case "remove":
             const cartItems=[...state.cartItems];
@@ -18,16 +37,40 @@ const cart = (state = initialState, action) => {
             return {
                 ...state,
                 cartItems,
-                price:state.price-action.payload.product.price
+                price:state.price-(action.payload.product.price*action.payload.product.quantity)
             }
-            case "increase":
-                const temp=[...state.cartItems];
-                temp[action.payload.index].quantity++;
-                return {
+        case "increase":
+            const temp=[...state.cartItems];
+            temp[action.payload.index].quantity++;
+            const tempPrice=temp[action.payload.index].price;
+            return {
                     ...state,
                     cartItems:temp,
-                    //price:state.price+cartItems[action.payload.index].price
+                    price:state.price+tempPrice
                 }
+        case "decrease":
+            const temp1=[...state.cartItems];
+            if(temp1[action.payload.index].quantity===1)
+            {   
+                const tempPrice1=temp1[action.payload.index].price;
+                temp1.splice(action.payload.index,1);
+                return {
+                    ...state,
+                    cartItems:temp1,
+                    price:state.price-tempPrice1
+                }
+            }
+            if(temp1[action.payload.index].quantity>1)
+            {
+                temp1[action.payload.index].quantity--;
+                const tempPrice1=temp1[action.payload.index].price;
+                return {
+                    ...state,
+                    cartItems:temp1,
+                    price:state.price-tempPrice1
+                    }
+            }
+            
         default: 
             return state
     }
