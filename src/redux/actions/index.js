b3bffd567348d77715fd2882d9d69401b60c57e5
Arif <muhammadarif734@gmail.com>
2021-09-26 17:44:13 -0400

    checkout functionality

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -2,12 +2,14 @@ import FetchProductAction from "./fetch.product.action";
 import FetchCategoriesAction from "./fetch.categories.action";
 import fetchProductDetailsAction from "./fetch.product.details.action";
 import CartAction from "./cart.action";
+import chekoutAction from "./checkout.action";
 
 const allActions = {
   FetchProductAction,
   FetchCategoriesAction,
   fetchProductDetailsAction,
   CartAction,
+  chekoutAction,
 };
 
 export default allActions;
